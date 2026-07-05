aeddc4b3887eb73f3f44ae3b35d386b646b5294b
franciscamac <franpms89@gmail.com>
2026-07-04 22:23:49 -0300

    add produtos

---
+++ produtos/src/test/java/io/github/ms_icompras/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ms_icompras.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
