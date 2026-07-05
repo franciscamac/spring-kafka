c1280a2e262923a9471bc177b24dae01952265a8
franciscamac <franpms89@gmail.com>
2026-07-04 22:25:01 -0300

    add icompras-servicos

---
+++ icompras-servicos/src/test/java/io/github/ms_icompras/servicos/ServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ms_icompras.servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
