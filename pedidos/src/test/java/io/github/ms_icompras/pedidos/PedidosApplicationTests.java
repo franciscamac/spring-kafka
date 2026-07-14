e8593003c518ae20262ec9087117f452395e459b
franciscamac <franpms89@gmail.com>
2026-07-13 21:36:42 -0300

    adicionando pedidos

---
+++ pedidos/src/test/java/io/github/ms_icompras/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ms_icompras.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
